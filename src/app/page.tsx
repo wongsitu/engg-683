29525de026a6070bbfa049f3050bd7b5defdab2e
wongsitu <sa_waika_4d_23@hotmail.com>
2023-06-11 22:36:45 -0600

    feat: info dummy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,13 +57,12 @@ function App() {
           </nav>
         </div>
       </header>
-
       <section className="bg-white py-20">
         <div className="container mx-auto px-4">
           <div className="md:flex md:items-center">
             <div className="md:w-1/2">
-              <h1 className="font-bold text-4xl md:text-6xl mb-4 text-gray-700">Welcome to Oil & Gas Company</h1>
-              <p className="text-gray-700 text-xl mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia faucibus posuere. Sed elementum nisi neque, et efficitur justo sagittis vitae.</p>
+              <h1 className="font-bold text-4xl md:text-6xl mb-4 text-gray-700">Welcome to MPMFW</h1>
+              <p className="text-gray-700 text-xl mb-8">Your premier destination for reliable and sustainable energy solutions in the oil and gas industry. As a leader in the field, we are committed to excellence, innovation, and environmental stewardship.</p>
               <a href="#services" className="inline-block bg-black text-white py-3 px-6 rounded-lg text-xl font-semibold hover:bg-gray-800">Learn More</a>
             </div>
             <div className="md:w-1/2 mt-10 md:mt-0">
@@ -72,7 +71,6 @@ function App() {
           </div>
         </div>
       </section>
-
       <section id="services" className="bg-sky-900 py-20">
         <div className="container mx-auto px-4">
           <h2 className="text-4xl font-bold mb-10">Our Services</h2>
@@ -81,42 +79,42 @@ function App() {
               <div className="bg-white p-6 rounded-lg shadow">
                 <Image src={digitalTwins} alt="Digital twins" className="w-full rounded-lg mb-4" />
                 <h3 className="text-2xl font-bold mb-2 text-gray-700">Digital twins</h3>
-                <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="text-gray-700">Revolutionize operations with virtual replicas for real-time monitoring and optimization.</p>
               </div>
             </div>
             <div className="px-4 mb-8 break-inside-avoid-column">
               <div className="bg-white p-6 rounded-lg shadow">
                 <Image src={cloudSolutions} alt="Cloud Solutions" className="w-full rounded-lg mb-4" />
                 <h3 className="text-2xl font-bold mb-2 text-gray-700">Cloud integrated solutions</h3>
-                <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="text-gray-700">Secure, scalable, and accessible solutions for optimized operations and data management.</p>
               </div>
             </div>
             <div className="px-4 mb-8 break-inside-avoid-column">
               <div className="bg-white p-6 rounded-lg shadow">
                 <Image src={artificialIntelligence} alt="AI-driven assistance" className="w-full rounded-lg mb-4" />
                 <h3 className="text-2xl font-bold mb-2 text-gray-700">AI-driven assistance</h3>
-                <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="text-gray-700">Gain insights, automate processes, and make informed decisions with intelligent AI solutions.</p>
               </div>
             </div>
             <div className="px-4 mb-8 break-inside-avoid-column">
               <div className="bg-white p-6 rounded-lg shadow">
                 <Image src={realTimeData} alt="Real Time data" className="w-full rounded-lg mb-4" />
                 <h3 className="text-2xl font-bold mb-2 text-gray-700">Real-time data</h3>
-                <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="text-gray-700">Access critical information instantly and make informed decisions with live data feeds.</p>
               </div>
             </div>
             <div className="px-4 mb-8 break-inside-avoid-column">
               <div className="bg-white p-6 rounded-lg shadow">
                 <Image src={sensors} alt="E2E sensor installation" className="w-full rounded-lg mb-4" />
                 <h3 className="text-2xl font-bold mb-2 text-gray-700">E2E sensor monitoring</h3>
-                <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="text-gray-700">Enhance reliability through real-time data collection and proactive maintenance.</p>
               </div>
             </div>
             <div className="px-4 mb-8 break-inside-avoid-column">
               <div className="bg-white p-6 rounded-lg shadow">
                 <Image src={augmentedReality} alt="E2E sensor installation" className="w-full rounded-lg mb-4" />
                 <h3 className="text-2xl font-bold mb-2 text-gray-700">Augmented reality</h3>
-                <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="text-gray-700">Enhance task performance and situational awareness with immersive augmented reality technology.</p>
               </div>
             </div>
           </div>
@@ -136,13 +134,12 @@ function App() {
               </div>
             </div>
             <div className="md:w-1/2 px-4 flex flex-col justify-center">
-              <p className="text-xl mb-8 text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum eleifend mauris, vitae commodo nunc bibendum eget. Fusce tristique, magna nec mollis malesuada, mi nunc pellentesque nisl, sed congue turpis nisi non enim. In vel magna sem. Pellentesque viverra interdum turpis, at ultrices odio vehicula ut. Curabitur nec suscipit nisl, ac hendrerit lorem.</p>
-              <p className="text-xl text-gray-700">Phasellus vitae purus ac libero faucibus efficitur vitae et erat. Mauris vitae felis cursus, cursus turpis non, cursus leo. Fusce sed sollicitudin est. Nunc eget tristique ligula, eget hendrerit magna. Aliquam ullamcorper purus id quam vulputate, non aliquam purus lobortis.</p>
+              <p className="text-xl mb-8 text-gray-700">Our company is driven by a talented and dedicated team of professionals who are passionate about the oil and gas industry and committed to achieving excellence in everything they do. With their expertise, experience, and unwavering determination, our team is at the forefront of innovation and ensures the success of our company.</p>
+              <p className="text-xl text-gray-700">At MPMFW, we believe that our team is our greatest asset. Their passion, expertise, and collaborative spirit are the driving force behind our success in the oil and gas industry. Together, we are committed to achieving excellence, delivering value to our clients, and making a positive impact in the energy sector.</p>
             </div>
           </div>
         </div>
       </section>
-
       <section id="contact" className="bg-sky-900 py-20">
         <div className="container mx-auto px-4">
           <h2 className="text-4xl font-bold mb-10">Contact Us</h2>
@@ -150,15 +147,15 @@ function App() {
             <div className="md:w-1/2 px-4 mb-8">
               <form>
                 <div className="mb-4">
-                  <label className="block text-gray-700 text-xl font-bold mb-2" htmlFor="name">Name</label>
+                  <label className="block text-xl font-bold mb-2" htmlFor="name">Name</label>
                   <input type="text" id="name" className="w-full py-3 px-4 rounded-lg border border-gray-300" placeholder="Your Name" />
                 </div>
                 <div className="mb-4">
-                  <label className="block text-gray-700 text-xl font-bold mb-2" htmlFor="email">Email</label>
+                  <label className="block text-xl font-bold mb-2" htmlFor="email">Email</label>
                   <input type="email" id="email" className="w-full py-3 px-4 rounded-lg border border-gray-300" placeholder="Your Email" />
                 </div>
                 <div className="mb-4">
-                  <label className="block text-gray-700 text-xl font-bold mb-2" htmlFor="message">Message</label>
+                  <label className="block text-xl font-bold mb-2" htmlFor="message">Message</label>
                   <textarea id="message" className="w-full py-3 px-4 rounded-lg border border-gray-300 h-32 resize-none" placeholder="Your Message"></textarea>
                 </div>
                 <button type="submit" className="bg-black text-white py-3 px-6 rounded-lg text-xl font-semibold hover:bg-gray-800">Send Message</button>
@@ -166,20 +163,19 @@ function App() {
             </div>
             <div className="md:w-1/2 px-4">
               <p className="text-xl mb-4">123 Main Street</p>
-              <p className="text-xl mb-4">City, State</p>
-              <p className="text-xl mb-4">Country</p>
+              <p className="text-xl mb-4">Calgary, Alberta</p>
+              <p className="text-xl mb-4">Canada</p>
               <p className="text-xl mb-4">Phone: (123) 456-7890</p>
-              <p className="text-xl">Email: info@company.com</p>
+              <p className="text-xl">Email: info@mpmfw.com</p>
             </div>
           </div>
         </div>
       </section>
-
       <footer className="bg-gray-800 py-10">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap items-center justify-between">
             <div className="w-full md:w-auto text-center md:text-left mb-4 md:mb-0">
-              <span className="text-white">© 2023 Oil & Gas Company. All rights reserved.</span>
+              <span className="text-white">© 2023 MPMFW. All rights reserved.</span>
             </div>
             <div className="w-full md:w-auto text-center md:text-right">
               <a href="#privacy" className="text-gray-400 hover:text-white mr-4">Privacy Policy</a>
